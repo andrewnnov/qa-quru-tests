710a66cae26a07785b07d3abd1f0e02c9e9c2cd9
andrewnnov <andrewnnov@yandex.ru>
2023-08-30 20:29:58 -0300

    add method

--- src/test/java/com/github/SelenideRepositorySearch.java
+++ src/test/java/com/github/SelenideRepositorySearch.java
@@ -1,6 +1,7 @@
 package com.github;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class SelenideRepositorySearch {
         //arrange
         //act
         //assert
-        Selenide.open("https://github.com/");
+        open("https://github.com/");
 
         $("[type=button].flex-1").click();
         $("#query-builder-test").click();
@@ -21,10 +22,15 @@ public class SelenideRepositorySearch {
         $$(".hKtuLA").first().$("a").click();
         //перевод строки идентичен пробелу
         $("#repository-container-header").shouldHave(Condition.text("selenide / selenide"));
+    }
 
-
-
-
-
+    @Test
+    void solntsevShouldBeOk() {
+        open("https://github.com/selenide/selenide");
+        $(".BorderGrid").$(Selectors.byText("Contributors")).ancestor(".BorderGrid-row")
+                .$$("ul li").first().hover();
+        sleep(5000);
+        //$$(".Truncate a").first().shouldHave(Condition.text("Andrei Solntcev"));
+        $(".Truncate-text a").shouldHave(Condition.text("Andrei Solntsev"));
     }
 }
